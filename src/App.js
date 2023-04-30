8c512519dfc5be921bab0b11bce54b465de637d5
AndreaCheah <104676203+AndreaCheah@users.noreply.github.com>
2023-04-30 19:09:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ import SearchBar from './components/SearchBar';
 import ThemeSelector from './components/ThemeSelector';
 import { useTheme } from './hooks/useTheme';
 import Admin from './pages/admin/Admin';
-
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase/config';
 
 function App() {
+  const [data, setData] = useState([])
   const{mode} = useTheme()
+
+  useEffect(() => {
+    const ref = collection(db, 'displayed');
+    getDocs(ref).then((snapshot) => {
+      const results = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setData(results);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, []);
+
   return (
     <div className= {`App ${mode}`}>
      <BrowserRouter>
